608a316f533573a4805e6df757d4c0da0bdeb4b1
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-12-29 21:34:33 +0700

    add basic structure

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>Markdown Previewer</div>
+      <label htmlFor="editor">Editor</label>
+      <button>Expand</button>
+      <textarea id="editor" cols="30" rows="10"></textarea>
+      <label htmlFor="previewer">Previewer</label>
+      <textarea id="previewer" cols="30" rows="10"></textarea>
+      <button>Expand</button>
+    </>
+  )
 }
 
-export default App;
+export default App

e594bf4cecd798f8176a9b1115ae362e760c5139
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-12-29 22:48:42 +0700

    implement markdown parser

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,12 +1,30 @@
+import { marked } from 'marked'
+import { useState } from 'react'
+import React from 'react'
+
 const App = () => {
+  const [text, setText] = useState('')
+
+  const renderElements = () => {
+    const html = marked.parse(text)
+    return (
+      <div dangerouslySetInnerHTML={{__html: html}}></div>
+    )
+  }
+  
   return (
     <>
       <div>Markdown Previewer</div>
       <label htmlFor="editor">Editor</label>
       <button>Expand</button>
-      <textarea id="editor" cols="30" rows="10"></textarea>
+      <textarea
+        id="editor"
+        cols="30"
+        rows="10"
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
       <label htmlFor="previewer">Previewer</label>
-      <textarea id="previewer" cols="30" rows="10"></textarea>
+      <div>{renderElements()}</div>
       <button>Expand</button>
     </>
   )

d24997cb4733ead9bbb447ab8607922cbd49ff41
Michael Nguyen <michaelnguyen.creative@gmail.com>
2022-12-30 22:11:27 +0700

    add panel expanding button

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -5,39 +5,45 @@ import initialTextPath from './initialText.md'
 
 const App = () => {
   const [text, setText] = useState('')
+  const [showEditor, setShowEditor] = useState(false)
+  const [showPreviewer, setShowPreviewer] = useState(false)
 
   useEffect(() => {
     fetch(initialTextPath)
-      .then(res => res.text())
-      .then(text => {
+      .then((res) => res.text())
+      .then((text) => {
         console.log('md', text)
         setText(text)
       })
   })
 
+  useEffect(() => {}, [showEditor, showPreviewer])
+
   const renderElements = () => {
     // console.log('text', text)
     const html = marked.parse(text)
     // console.log('html', html)
-    return (
-      <div id="preview" dangerouslySetInnerHTML={{__html: html}}></div>
-    )
+    return <div id="preview" dangerouslySetInnerHTML={{ __html: html }}></div>
   }
-  
+
+  // To-do: Implement panel expansion functionality
   return (
     <>
-      <div>Markdown Previewer</div>
-      <label htmlFor="editor">Editor</label>
-      <button>Expand</button>
-      <textarea
-        id="editor"
-        cols="30"
-        rows="10"
-        defaultValue={text}
-        onChange={(e) => setText(e.target.value)}
-      ></textarea>
-      <div>Previewer{renderElements()}</div>
-      <button>Expand</button>
+      <div style={{ display: !showEditor && 'none' }}>
+        <label htmlFor="editor">Editor</label>
+        <textarea
+          id="editor"
+          cols="30"
+          rows="10"
+          defaultValue={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+      </div>
+      <button onClick={() => setShowEditor(!showEditor)}>Expand</button>
+      <div style={{ display: !showPreviewer && 'none' }}>
+        Previewer{renderElements()}
+      </div>
+      <button onClick={() => setShowPreviewer(!showPreviewer)}>Expand</button>
     </>
   )
 }

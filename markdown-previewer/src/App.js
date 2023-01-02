0eba01f0d95fe6dfac920509aae855489fc50997
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-01-02 17:25:49 +0700

    clean up unneccesary code

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -5,32 +5,25 @@ import initialTextPath from './initialText.md'
 
 const App = () => {
   const [text, setText] = useState('')
-  const [showEditor, setShowEditor] = useState(false)
-  const [showPreviewer, setShowPreviewer] = useState(false)
 
   useEffect(() => {
     fetch(initialTextPath)
       .then((res) => res.text())
       .then((text) => {
-        console.log('md', text)
         setText(text)
       })
   })
 
-  useEffect(() => {}, [showEditor, showPreviewer])
-
   const renderElements = () => {
-    // console.log('text', text)
     marked.use({ breaks: true })
     const html = marked.parse(text)
-    // console.log('html', html)
     return <div id="preview" dangerouslySetInnerHTML={{ __html: html }}></div>
   }
 
   // To-do: Implement panel expansion functionality
   return (
     <>
-      <div style={{ display: !showEditor && 'none' }}>
+      <div>
         <label htmlFor="editor">Editor</label>
         <textarea
           id="editor"
@@ -39,12 +32,15 @@ const App = () => {
           defaultValue={text}
           onChange={(e) => setText(e.target.value)}
         ></textarea>
+        <button>Expand</button>
       </div>
-      <button onClick={() => setShowEditor(!showEditor)}>Expand</button>
-      <div style={{ display: !showPreviewer && 'none' }}>
-        Previewer{renderElements()}
+      <div>
+        <div>
+          Previewer
+          <button>Expand</button>
+        </div>
+        {renderElements()}
       </div>
-      <button onClick={() => setShowPreviewer(!showPreviewer)}>Expand</button>
     </>
   )
 }
